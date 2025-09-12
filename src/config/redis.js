a98433060f55b4a744a9af1d0fda62573065d85f
Satvik <satviksharma3123@gmail.com>
2025-09-13 00:46:20 +0530

    feat: Implement OTP email service and update Redis configuration

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,19 +1,17 @@
-
 import Redis from 'ioredis';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const redis = new Redis(process.env.UPSTASH_REDIS_REST_URL, {
-	password: process.env.UPSTASH_REDIS_REST_TOKEN,
-	tls: {},
+const redis = new Redis(process.env.REDIS_URL, {
+  tls: {}, // Required for Upstash TLS connection
 });
 
 redis.on('connect', () => {
-	console.log('Connected to Upstash Redis!');
+  console.log('Connected to Upstash Redis!');
 });
 
 redis.on('error', (err) => {
-	console.error('Redis connection error:', err);
+  console.error('Redis connection error:', err);
 });
 
 export default redis;
